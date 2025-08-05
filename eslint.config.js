7824f4e6620cac4095d1e8d1e1e08e67693db197
Jaime Pineda <jpin730@pm.me>
2025-08-05 11:44:41 -0600

    chore: :package: add eslint, prettier, husky and lint-staged config

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import { fileURLToPath } from 'node:url'
+import { includeIgnoreFile } from '@eslint/compat'
+import astro from 'eslint-plugin-astro'
+import globals from 'globals'
+import js from '@eslint/js'
+import prettier from 'eslint-plugin-prettier/recommended'
+import tseslint from 'typescript-eslint'
+
+const gitignorePath = fileURLToPath(new URL('.gitignore', import.meta.url))
+
+export default [
+  includeIgnoreFile(gitignorePath),
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...astro.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      'no-console': 'error',
+    },
+  },
+  prettier,
+]
